79f77fdcbd245af05abf69dbe6180e49dac9d2df
masterajima <98704309+masterajima@users.noreply.github.com>
2022-03-26 19:24:56 -0400

    add participants

---
+++ Room.js
@@ -0,0 +1,22 @@
+//import state and model from node library
+import React,{useEffect,useState} from "react";
+import video from "twilio-video";
+import Participant from "./Participant"
+const Room=({roomName,token,handleLogout})=>{
+    const[room,setRoom]=useState(null);
+    const[participants,setParticipants]=useState([]);
+}
+//if already connected participants
+useEffect(()=>{
+                 const participantConnected=(participant)=>{
+                    setParticipants((prevParticipants)=>[...prevParticipants,participant]);
+                }
+            });
+            //check which participant are connected to stress
+let participantConnected=(participants)=>{
+    setParticipants((prevParticipants)=>{
+        prevParticipants.filter((p)=>p!==participants)
+    
+    ;}
+    );}
+Video.connect(token,{name:roomName})
